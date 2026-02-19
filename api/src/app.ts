b50e4e27b77464507db7eeb8123c6877febd3eb3
Bishal <mr.shrestha713@gmail.com>
2026-02-19 14:40:09 +0545

    feat: add agent router

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import type { Express } from "express";
+import agentRouter from "./routes/agent.routes";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use("/", agentRouter);
 
 export default app;
